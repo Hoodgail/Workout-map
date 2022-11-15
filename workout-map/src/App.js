9f531af9921ec3f92ef2eec54d05d2d9c0c6927e
Aaron Liu <aaron155658@gmail.com>
2022-11-15 09:23:07 -0500

    ui layout

--- workout-map/src/App.js
+++ workout-map/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Map from './components/Map';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div>
-      <Sidebar></Sidebar>
-      <Map></Map>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={4}>
+          <Sidebar></Sidebar>
+        </Col>
+        <Col sm={8}>
+          <Map></Map>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- workout-map/src/components/Sidebar/index.js
+++ workout-map/src/components/Sidebar/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Logo from '../../assets/logo.png';
-import Icon from '../../assets/icon.png';
 
 function Sidebar() {
   // function
