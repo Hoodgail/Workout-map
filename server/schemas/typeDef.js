afca4356f16a7385e08557f40efd2ae941f4409f
Aaron Liu <aaron155658@gmail.com>
2022-11-16 16:29:10 -0500

    add JWT

--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -29,6 +29,7 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
@@ -38,6 +39,9 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
   type Auth {
     token: ID!
