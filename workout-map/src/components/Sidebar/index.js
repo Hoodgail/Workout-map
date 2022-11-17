ad3c042d26a58f3d9ebf5b179f62cac000818473
Aaron Liu <aaron155658@gmail.com>
2022-11-17 17:22:11 -0500

    style page

--- workout-map/src/components/Sidebar/index.js
+++ workout-map/src/components/Sidebar/index.js
@@ -1,62 +1,20 @@
 import React from 'react';
-import Logo from '../../assets/logo.png';
+import Login from '../Login';
 
 function Sidebar() {
   // function
 
   return (
-    <div class="sidebar">
-      <img src={Logo} alt="Logo" class="logo" />
-
-      <ul class="workouts">
-        <form class="form hidden">
-          <div class="form__row">
-            <label class="form__label">Type</label>
-            <select class="form__input form__input--type">
-              <option value="running">Running</option>
-              <option value="cycling">Cycling</option>
-            </select>
-          </div>
-          <div class="form__row">
-            <label class="form__label">Distance</label>
-            <input class="form__input form__input--distance" placeholder="km" />
-          </div>
-          <div class="form__row">
-            <label class="form__label">Duration</label>
-            <input
-              class="form__input form__input--duration"
-              placeholder="min"
-            />
-          </div>
-          <div class="form__row">
-            <label class="form__label">Cadence</label>
-            <input
-              class="form__input form__input--cadence"
-              placeholder="step/min"
-            />
-          </div>
-          <div class="form__row form__row--hidden">
-            <label class="form__label">Elev Gain</label>
-            <input
-              class="form__input form__input--elevation"
-              placeholder="meters"
-            />
-          </div>
-          <button class="form__btn">OK</button>
-        </form>
+    <div className="sidebar">
+      <ul className="workouts">
+        <Login />
       </ul>
 
-      <p class="copyright">
+      <p className="copyright">
         &copy; Copyright by
-        <a
-          class="twitter-link"
-          target="_blank"
-          href="https://twitter.com/jonasschmedtman"
-        >
-          Jonas Schmedtmann
-        </a>
-        . Use for learning or your portfolio. Don't use to teach. Don't claim as
-        your own.
+        <a href="https://github.com/AaronLiuXM"> Aaron</a>,
+        <a href="https://github.com/Hoodgail"> Hoodley</a>,
+        <a href="https://github.com/srich92"> Shantel</a>
       </p>
     </div>
   );
---
+++ workout-map/src/utils/queries.js
@@ -0,0 +1,22 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
+    }
+  }
+`;
